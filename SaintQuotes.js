acf0000bf6261215c5ed0e0a022643146b8e7b95
Sarah Korkis <Sarahkorkis@gmail.com>
2022-08-26 21:07:03 -0400

    Add files via upload

---
+++ SaintQuotes.js
@@ -0,0 +1,311 @@
+const quotes = [{
+    quote: `"In times of desolation, God conceals Himself from us so that we can discover for ourselves what we are without Him."`,
+    writer: `– St. Margaret of Cortona`
+}, {
+    quote: `"To be taken with love for a soul, God does not look on its greatness, but the greatness of its humility."`,
+    writer: `– St. John of the Cross`
+}, {
+    quote: `"Who except God can give you peace? Has the world ever been able to satisfy the heart?"`,
+    writer: `– St. Gerard`
+}, {
+    quote: `"Your God is ever beside you — indeed, He is even within you."`,
+    writer: `– St. Alphonsus Ligouri`
+}, {
+    quote: `"To be a servant of Christ is to be truly free."`,
+    writer: `– St. Agatha`
+}, {
+    quote: `"Love God, serve God; everything is in that."`,
+    writer: `– St. Clare of Assisi`
+}, {
+    quote: `"Apart from the cross, there is no other ladder by which we may get to heaven."`,
+    writer: `– St. Rose of Lima`
+}, {
+    quote: `"Teach us to give and not count the cost."`,
+    writer: `– St. Ignatius de Loyola`
+}, {
+    quote: `"If you are what you should be, you will set the whole world ablaze!"`,
+    writer: `- St. Catherine of Sienna`
+}, {
+    quote: `"Pray, Hope, and Do not worry."`,
+    writer: `- St. Padre Pio`
+}, {
+    quote: `"Jesus, help me to simplify my life by learning what you want me to be and becoming that person."`,
+    writer: `- St. Therese`
+}, {
+    quote: `"Let nothing Disturb you, Let nothing frighten you, Though all things pass, God does not change. Patience wins all things. But he lacks nothing who possesses God; For God alone suffices."`,
+    writer: `- St. Teresa`
+},{
+    quote: `True charity consists in bearing all our neighbour’s defects – not being surprised at their weakness, but edified at their smallest virtues.`,
+    writer: `– St. Therese`
+}, {
+    quote: `If I did not simply live from one moment to another, it would be impossible for me to be patient; but I only look at the present, I forget the past, and I take good care not to forestall the future.`,
+    writer: `– St. Therese`
+}, {
+    quote: `"Do you realise that Jesus is there in the tabernacle expressly for you – for you alone? He burns with the desire to come into your heart… Don’t listen to the demon; laugh at him, and go without fear to receive the Jesus of peace and love.`,
+    writer: `– St. Therese`
+}, {
+    quote: `The guest of our soul knows our misery; He comes to find an empty tent within us – that is all He asks.`,
+    writer: `– St. Therese`
+}, {
+    quote: `"Holiness consists simply in doing God’s will, and being just what God wants us to be.`,
+    writer: `– St. Therese`
+}, {
+    quote: `"When I die, I will send down a shower of roses from the heavens, I will spend my heaven by doing good on earth."`,
+    writer: `– St. Therese`
+}, {
+    quote: `"The good God does not need years to accomplish His work of love in a soul; one ray from His Heart can, in an instant, make His flower bloom for eternity.`,
+    writer: `– St. Therese`
+}, {
+    quote: `"It pleases Him to create great Saints, who may be compared with the lilies or the rose; but He has also created little ones, who must be content to be daisies or violets, nestling at His feet to delight His eyes when He should choose to look at them. The happier they are to be as He wills, the more perfect they are."`,
+    writer: `– St. Therese`
+}, {
+    quote: `"Our Lord’s love shines out just as much through a little soul who yields completely to His Grace as it does through the greatest."`,
+    writer: `- St. Therese`
+}, {
+    quote: `"Jesus has chosen to show me the only way which leads to the Divine Furnace of love; it is the way of childlike self-surrender, the way of a child who sleeps, afraid of nothing, in its father’s arms."`,
+    writer: `- St. Therese`
+}, {
+    quote: `"Jesus, help me to simplify my life by learning what you want me to be and becoming that person."`,
+    writer: `- St. Therese`
+}, {
+    quote: `"Without love, deeds, even the most brilliant, count as nothing."`,
+    writer: `- St. Therese`
+},{
+    quote: `"In trial or difficulty I have recourse to Mother Mary, whose glance alone is enough to dissipate every fear."`,
+    writer: `- St. Therese`
+},{
+    quote: `"For me, prayer is a surge of the heart; it is a simple look turned toward heaven, it is a cry of recognition and of love, embracing both trial and joy."`,
+    writer: `- St. Therese`
+},{
+    quote: `"Let nothing Disturb you, Let nothing frighten you, Though all things pass, God does not change. Patience wins all things. But he lacks nothing who possesses God; For God alone suffices."`,
+    writer: `– St. Teresa of Avila`
+}, {
+    quote: `"Trust God that you are exactly where you are meant to be."`,
+    writer: `– St. Teresa Of Avila`
+}, {
+    quote: `"The surest way to determine whether one possesses the love of God is to see whether he or she loves his or her neighbor. These two loves are never separated. Rest assured, the more you progress in love of neighbor the more your love of God will increase."`,
+    writer: `– St. Teresa Of Avila`
+}, {
+    quote: `"When we accept what happens to us and make the best of it, we are praising God."`,
+    writer: `– St. Teresa Of Avila`
+}, {
+    quote: `"Prayer is an act of love. Words are not needed."`,
+    writer: `– St. Teresa Of Avila`
+}, {
+    quote: `"We always find that those who walked closest to Christ were those who had to bear the greatest trials."`,
+    writer: `– St. Teresa Of Avila`
+}, {
+    quote: `"Mental prayer in my opinion is nothing else than an intimate sharing between friends; it means taking time frequently to be alone with Him who we know loves us. The important thing is not to think much but to love much and so do that which best stirs you to love. Love is not great delight but desire to please God in everything."`,
+    writer: `– St. Teresa Of Avila`
+}, {
+    quote: `"Christ has no body now but mine. He prays in me, works in me, looks through my eyes, speaks through my words, works through my hands, walks with my feet and loves with my heart."`,
+    writer: `– St. Teresa Of Avila`
+}, {
+    quote: `"Each of us has a soul, but we forget to value it. We don't remember that we are creatures made in the image of God. We don't understand the great secrets hidden inside of us."`,
+    writer: `- St. Teresa Of Avila`
+}, {
+    quote: `"Desire to see God, be fearful of losing Him, and find joy in everything that can lead to Him. If you act in this way, you will always live in great peace."`,
+    writer: `- St. Teresa Of Avila`
+}, {
+    quote: `"The closer one approaches to God, the simpler one becomes."`,
+    writer: `- St. Teresa Of Avila`
+}, {
+    quote: `"Always think of yourself as everyone's servant; look for Christ Our Lord in everyone and you will then have respect and reverence for them all."`,
+    writer: `- Teresa Of Avila`
+},{
+    quote: `“Idleness is the enemy of the soul; and therefore the brethren ought to be employed in manual labor at certain times, at others, in devout reading.”`,
+    writer: `– St. Benedict`
+}, {
+    quote: `“The first degree of humility is prompt obedience.”`,
+    writer: `– St. Benedict`
+}, {
+    quote: `“He should first show them in deeds rather than words all that is good and holy.”`,
+    writer: `– St. Benedict`
+}, {
+    quote: `“It is time now for us to rise from sleep.”`,
+    writer: `– St. Benedict`
+}, {
+    quote: `“The prophet shows that, for the sake of silence, we are to abstain even from good talk. If this be so, how much more needful is it that we refrain from evil words, on account of the penalty of the sin!”`,
+    writer: `– St. Benedict`
+}, {
+    quote: `“He should know that whoever undertakes the government of souls must prepare himself to account for them.”`,
+    writer: `– St. Benedict`
+}, {
+    quote: '"The world offers you comfort, but you were not made for comfort.  You were made for greatness."',
+    writer: '- Pope Benedict XVI'
+}, {
+    quote: '"Nothing great is ever achieved without enduring much"',
+    writer: '- St. Catherine of Sienna'
+},{
+    quote: '"It isn’t good to let our thoughts disturb us or worry us at all."',
+    writer: '- St. Teresa of Ávila'
+},{
+    quote: '"The devil fears hearts on fire with love of God."',
+    writer: '- St. Catherine of Sienna'
+},{
+    quote: '"Hold your eyes on God and leave the doing to Him.  That is all the doing you have to worry about."',
+    writer: '- St. Jane Frances de Chantal'
+},{
+    quote: '"We do not know the number of souls that is ours to save through our prayers and sacrifices. therefore, let us always pray for sinners."',
+    writer: '- St. Faustina'
+},{
+    quote: '"Let us run to Mary, and, as her little children, cast ourselves into her arms with a perfect confidence."',
+    writer: '- St. Francis De Sales'
+},{
+    quote: '"Have patience with all things, but chiefly have patience with yourself. Do not lose courage in considering your own imperfections but instantly set about remedying them — every day begin the task anew."',
+    writer: '- St. Francis De Sales'
+},{
+    quote: '"Our entire good consists not only in accepting the truth of God’s word, but in persevering in it."',
+    writer: '- St. Francis De Sales'
+},{
+    quote: '"Make frequent visits to Jesus in the Blessed Sacrament and the devil will be powerless against you."',
+    writer: '- St. John Bosco'
+},{
+    quote: '"I asked you and you would not listen; so I asked my God and He did listen."',
+    writer: '- St. Scholastica'
+},{
+    quote: '"It is not sinners, but the wicked who should despair; it is not the magnitude of one’s crime, but contempt of God that dashes one’s hopes."',
+    writer: '- St. Peter Damian'
+},{
+    quote: '"Do not bestow your love on the world."',
+    writer: '-St. Gabriel Possenti'
+},{
+    quote: '"I will attempt day by day to break my will into pieces. I want to do God’s Holy Will, not my own!"',
+    writer: '- St. Gabriel Possenti'
+},{
+    quote: '"If there be a true way that leads to the Everlasting Kingdom, it is most certainly that of suffering, patiently endured."',
+    writer: '- St. Colette'
+},{
+    quote: '"Nothing seems tiresome or painful when you are working for a Master who pays well; who rewards even a cup of cold water given for love of Him."',
+    writer: '- St. Dominic Savio'
+},{
+    quote: '"If it were given to a man to see virtue’s reward in the next world, he would occupy his intellect, memory and will in nothing but good works, careless of danger or fatigue."',
+    writer: '- St. Catherine Of Genoa'
+},{
+    quote: '"Christ beside me, Christ before me, Christ behind me, Christ within me, Christ beneath me, Christ above me."',
+    writer: '- St. Patrick'
+},{
+    quote: '"In confession there is mercy. Believe it firmly, do not doubt, do not hesitate, never despair of the mercy of God."',
+    writer: '- St. Isidore Of Seville'
+},{
+    quote: '"Do you desire to study to your advantage? Let devotion accompany all your studies, and study less to make yourself learned than to become a saint. Consult God more than your books, and ask him, with humility, to make you understand what you read. Study fatigues and drains the mind and heart. Go from time to time to refresh them at the feet of Jesus Christ under his cross."',
+    writer: '- St. Vincent Ferrer'
+},{
+    quote: '"If you truly want to help the soul of your neighbor, you should approach God first with all your heart. Ask him simply to fill you with charity, the greatest of all virtues; with it you can accomplish what you desire."',
+    writer: '- St. Vincent Ferrer'
+},{
+    quote: '"Pride makes us forgetful of our eternal interests. It causes us to neglect totally the care of our soul."',
+    writer: '- St. John Baptist De La Salle'
+},{
+    quote: '"I shall love You, I shall love You always; when day breaks, when evening turns into night, at every hour, at every moment; I shall love You always, always, always."',
+    writer: '- St. Gemma Galgani'
+},{
+    quote: '"Nothing is anything more to me; everything is nothing to me, but Jesus: neither things nor persons, neither ideas nor emotions, neither honor nor sufferings. Jesus is for me honor, delight, heart and soul."',
+    writer: '- St. Bernadette'
+},{
+    quote: '"When the Holy Rosary is said well, it gives Jesus and Mary more glory and is more meritorious than any other prayer."',
+    writer: '- St. Louis De Montfort'
+},{
+    quote: '"You must know that when you ‘hail’ Mary, she immediately greets you! Don’t think that she is one of those rude women of whom there are so many — on the contrary, she is utterly courteous and pleasant. If you greet her, she will answer you right away and converse with you!"',
+    writer: '- St. Bernadine Of Siena'
+},{
+    quote: '"Bear the cross and do not make the cross bear you."',
+    writer: '- St. Philip Neri'
+},{
+    quote: '"You must never ask Jesus to wait."',
+    writer: '- St. Ursula Ledochowska'
+},{
+    quote: '"I am not afraid. I was born to do this."',
+    writer: '- St. Joan Of Arc'
+},{
+    quote: '"I place trust in God, my creator, in all things; I love Him with all my heart."',
+    writer: '- St. Joan Of Arc'
+},{
+    quote: '"Help yourself and God will help you."',
+    writer: '- St. Joan Of Arc'
+},{
+    quote: '"A great means to preserve continual peace and tranquillity of soul is to receive everything from the hands of God, both great and small, and in whatever way it comes."',
+    writer: '- St. Dorotheus'
+},{
+    quote: '"Virtues are formed by prayer. Prayer preserves temperance. Prayer suppresses anger. Prayer prevents emotions of pride and envy. Prayer draws into the soul the Holy Spirit, and raises man to Heaven."',
+    writer: '- St. Ephrem'
+},{
+    quote: '"Our Lord wills that you cling to Him alone! If your faith were greater how much more peaceful you would be even when great trials surround and oppress you."',
+    writer: '- St. Paula Frassinetti'
+},{
+    quote: '"Christians must lean on the Cross of Christ just as travelers lean on a staff when they begin a long journey."',
+    writer: '- St. Anthony Of Padua'
+},{
+    quote: '"Earthly riches are like the reed. Its roots are sunk in the swamp, and its exterior is fair to behold; but inside it is hollow. If a man leans on such a reed, it will snap off and pierce his soul."',
+    writer: '- St. Anthony Of Padua'
+},{
+    quote: '"Attribute to God every good that you have received. If you take credit for something that does not belong to you, you will be guilty of theft."',
+    writer: '- St. Anthony Of Padua'
+},{
+    quote: '"Since God does so much for me, what could I not do for him?"',
+    writer: '- St. Emily De Vialar'
+},{
+    quote: '"It is better to be the child of God than king of the whole world"',
+    writer: '- St. Aloysius Gonzaga'
+},{
+    quote: '"Occupy your minds with good thoughts, or the enemy will fill them with bad ones. Unoccupied, they cannot be."',
+    writer: '- St. Thomas More'
+},{
+    quote: '"Peace is not just the absence of war. Like a cathedral, peace must be constructed patiently and with unshakable faith."',
+    writer: '- St. Pope John Paul II'
+},{
+    quote: '"Our souls may lose their peace and even disturb other people’s, if we are always criticizing trivial actions—which are often not real defects at all, but we construed them wrongly through our ignorance of their motives."',
+    writer: '- St. Teresa Of Avila'
+},{
+    quote: '"Never be in a hurry; do everything quietly and in a calm spirit. Do not lose your inner peace for anything whatsoever, even if your whole world seems upset."',
+    writer: '- St. Francis De Sales'
+},{
+    quote: '"Go to the tabernacle in spirit when you are not able to do so physically!"',
+    writer: '- St. Padre Pio'
+},{
+    quote: '"Jesus Christ, Lord of all things! You see my heart, you know my desires. Possess all that I am—you alone. I am your sheep; make me worthy to overcome the devil."',
+    writer: '- St. Agatha'
+},{
+    quote: '"He who trusts in himself is lost. He who trusts in God can do all things."',
+    writer: '– St. Alphonsus Liguori'
+},{
+    quote: '"Do not lose heart, even if you should discover that you lack qualities necessary for the work to which you are called. He who called you will not desert you, but the moment you are in need he will stretch out his saving hand."',
+    writer: '– St. Angela Merici'
+},{
+    quote: '"No one will have any other desire in heaven than what God wills; and the desire of one will be the desire of all; and the desire of all and of each one will also be the desire of God."',
+    writer: '- St. Anselm'
+},{
+    quote: '"Nowhere other than looking at himself in the mirror of the Cross can man better understand how much he is worth."',
+    writer: '– St. Anthony of Padua'
+},{
+    quote: '"As the sun surpasses all the stars in lustre, so the sorrows of Mary surpass all the tortures of the martyrs."',
+    writer: '– St. Basil the Great'
+},]
+
+
+/**{
+    quote: '""',
+    writer: ''
+},] **/
+
+
+let btn = document.querySelector("#Qbtn");
+
+let quote = document.querySelector(".quote");
+
+let writer = document.querySelector(".writer");
+
+
+
+
+
+
+btn.addEventListener("click", function() {
+    let random = Math.floor(Math.random() * quotes.length);
+    
+    
+    quote.innerHTML = quotes[random].quote;
+
+    
+    writer.innerHTML = quotes[random].writer;
+})
