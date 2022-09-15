8c5fc8f0c06f5a6d51c8488306fc89b99f4596ac
Sarah Korkis <Sarahkorkis@gmail.com>
2022-09-15 17:56:47 -0400

    Update novenaDates.js

--- novenaDates.js
+++ novenaDates.js
@@ -21,9 +21,9 @@ window.onload = function NovenaFunction(){
 
     function MaryOfKnots(){
         Novena1.innerHTML = 'Mary UnDoer Of Knots <br> September 20 - 28'
-        Novena1.style.left = '8vw';
+        Novena1.style.left = '7vw';
         Novena1A.innerHTML = 'Starts the 20th!'
-        Novena1A.style.left = '8vw'
+        Novena1A.style.left = '12vw'
         Novena1sub.innerHTML = 'Feast Day <br> September 28'
         Novena1subA.innerHTML = 'Virgin Mary, Mother of fair love, Mother who never refuses to come to the aid of a child in need, Mother whose hands never cease to serve your beloved children because they are moved by the divine love and immense mercy that exists in your heart, cast your compassionate eyes upon me and see the snarl of knots that exist in my life. You know very well how desperate I am, my pain and how I am bound up by the Knots. Mary, Mother to whom God entrusted the undoing of the knots in the lives of his children, I entrust into your hands the ribbon of my life. No one, not even the Evil One himself, can take it away from your precious care. In your hands there is no knot that cannot be undone. Powerful Mother, by your grace and intercessory power with Your Son and My Liberator, Jesus, take into your hands today this knot… I beg you to undo it for the glory of god, once and for all. You are my hope. O my Lady, you are the only consolation God gives me, the fortification of my feeble strength, the enrichment of my destitution and with Christ the freedom from my chains. Hear my plea Keep me, guide me, protect me, o safe refuge. <br> (Mention your request here…) <br> Say 1: Our Father… Say 1: Hail Mary… Say 1: Glory Be…'
         novena1Pic.src = './MaryUndoer.png';
