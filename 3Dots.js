54719009723ff36e7d652030de79f84427e65ec1
Sarah Korkis <Sarahkorkis@gmail.com>
2022-09-22 12:40:12 -0400

    Add files via upload

---
+++ 3Dots.js
@@ -0,0 +1,183 @@
+function switchToN1() {
+    var Novena1 = document.getElementById("Novena1");
+    var Novena1A = document.getElementById("Novena1A");
+    var N1Button = document.getElementById("N1Button");
+    var Novena1sub = document.getElementById("Novena1sub");
+    var Novena1subA = document.getElementById("Novena1subA");
+    var novena1Pic = document.getElementById("novena1Pic");
+
+    var scrollButtons = document.getElementById("scrollButtons");
+    var firstScrollDot = document.getElementById("firstDotButton");
+    var secondScrollDot = document.getElementById("secondDotButton");
+    var thirdScrollDot = document.getElementById("thirdDotButton");
+
+    var Novena2 = document.getElementById("Novena2");
+    var Novena2A = document.getElementById("Novena2A");
+    var N2Button = document.getElementById("N2Button");
+    var Novena2sub = document.getElementById("Novena2sub");
+    var Novena2subA = document.getElementById("Novena2subA");
+    var novena2Pic = document.getElementById("novena2Pic");
+
+    var Novena3 = document.getElementById("Novena3");
+    var Novena3A = document.getElementById("Novena3A");
+    var N3Button = document.getElementById("N3Button");
+    var Novena3sub = document.getElementById("Novena3sub");
+    var Novena3subA = document.getElementById("Novena3subA");
+    var novena3Pic = document.getElementById("novena3Pic");
+
+    var roses = document.getElementById("Roses");
+    var roses2 = document.getElementById("Roses2");
+    var roses3 = document.getElementById("Roses3");
+    var roses4 = document.getElementById("Roses4");
+    var roses5 = document.getElementById("Roses5");
+
+      Novena1.style.display = "inline";
+      Novena1sub.style.display = "inline";
+      N1Button.style.display = "inline";
+      novena1Pic.style.display = "inline";
+      novena1Pic.src = './MaryUndoer.png';
+
+      Novena2.style.display = "none";
+      Novena2sub.style.display = "none";
+      N2Button.style.display = "none";
+      novena2Pic.style.display = "none"
+
+      Novena3.style.display = "none";
+      Novena3sub.style.display = "none";
+      N3Button.style.display = "none";
+      novena3Pic.style.display = "none";
+
+
+      scrollButtons.style.display = "inline";
+      firstScrollDot.style.opacity = "100%";
+      secondScrollDot.style.opacity = "50%";
+      thirdScrollDot.style.opacity = "50%";
+
+      roses.style.animation = 'none';
+      roses2.style.animation = 'none';
+      roses3.style.animation = 'none';
+      roses4.style.animation = 'none';
+      roses5.style.animation = 'none';
+  }
+
+  function switchToN2() {
+    var Novena1 = document.getElementById("Novena1");
+    var Novena1A = document.getElementById("Novena1A");
+    var N1Button = document.getElementById("N1Button");
+    var Novena1sub = document.getElementById("Novena1sub");
+    var Novena1subA = document.getElementById("Novena1subA");
+    var novena1Pic = document.getElementById("novena1Pic");
+
+    var scrollButtons = document.getElementById("scrollButtons");
+    var firstScrollDot = document.getElementById("firstDotButton");
+    var secondScrollDot = document.getElementById("secondDotButton");
+    var thirdScrollDot = document.getElementById("thirdDotButton");
+
+    var Novena2 = document.getElementById("Novena2");
+    var Novena2A = document.getElementById("Novena2A");
+    var N2Button = document.getElementById("N2Button");
+    var Novena2sub = document.getElementById("Novena2sub");
+    var Novena2subA = document.getElementById("Novena2subA");
+    var novena2Pic = document.getElementById("novena2Pic");
+
+    var Novena3 = document.getElementById("Novena3");
+    var Novena3A = document.getElementById("Novena3A");
+    var N3Button = document.getElementById("N3Button");
+    var Novena3sub = document.getElementById("Novena3sub");
+    var Novena3subA = document.getElementById("Novena3subA");
+    var novena3Pic = document.getElementById("novena3Pic");
+
+    var roses = document.getElementById("Roses");
+    var roses2 = document.getElementById("Roses2");
+    var roses3 = document.getElementById("Roses3");
+    var roses4 = document.getElementById("Roses4");
+    var roses5 = document.getElementById("Roses5");
+
+        Novena2.style.display = "inline";
+        Novena2sub.style.display = "inline";
+        N2Button.style.display = "inline";
+        novena2Pic.style.display = "inline";
+        novena2Pic.src = './St. Michael Archangel.jpg';
+
+        Novena1.style.display = "none";
+        Novena1sub.style.display = "none";
+        novena1Pic.style.display = "none"
+        N1Button.style.display = "none";
+
+        Novena3.style.display = "none";
+        Novena3sub.style.display = "none";
+        N3Button.style.display = "none";
+        novena3Pic.style.display = "none";
+
+        scrollButtons.style.display = "inline";
+        firstScrollDot.style.opacity = "50%";
+        secondScrollDot.style.opacity = "100%";
+        thirdScrollDot.style.opacity = "50%";
+
+        roses.style.animation = 'none';
+        roses2.style.animation = 'none';
+        roses3.style.animation = 'none';
+        roses4.style.animation = 'none';
+        roses5.style.animation = 'none';
+    }
+
+    function switchToN3() {
+        var Novena1 = document.getElementById("Novena1");
+        var Novena1A = document.getElementById("Novena1A");
+        var N1Button = document.getElementById("N1Button");
+        var Novena1sub = document.getElementById("Novena1sub");
+        var Novena1subA = document.getElementById("Novena1subA");
+        var novena1Pic = document.getElementById("novena1Pic");
+
+        var scrollButtons = document.getElementById("scrollButtons");
+        var firstScrollDot = document.getElementById("firstDotButton");
+        var secondScrollDot = document.getElementById("secondDotButton");
+        var thirdScrollDot = document.getElementById("thirdDotButton");
+    
+        var Novena2 = document.getElementById("Novena2");
+        var Novena2A = document.getElementById("Novena2A");
+        var N2Button = document.getElementById("N2Button");
+        var Novena2sub = document.getElementById("Novena2sub");
+        var Novena2subA = document.getElementById("Novena2subA");
+        var novena2Pic = document.getElementById("novena2Pic");
+
+        var Novena3 = document.getElementById("Novena3");
+        var Novena3A = document.getElementById("Novena3A");
+        var N3Button = document.getElementById("N3Button");
+        var Novena3sub = document.getElementById("Novena3sub");
+        var Novena3subA = document.getElementById("Novena3subA");
+        var novena3Pic = document.getElementById("novena3Pic");
+
+        var roses = document.getElementById("Roses");
+        var roses2 = document.getElementById("Roses2");
+        var roses3 = document.getElementById("Roses3");
+        var roses4 = document.getElementById("Roses4");
+        var roses5 = document.getElementById("Roses5");
+    
+            Novena3.style.display = "inline";
+            Novena3sub.style.display = "inline";
+            N3Button.style.display = "inline";
+            novena3Pic.style.display = "inline";
+            novena3Pic.src = './Therese.jpg';
+
+            Novena1.style.display = "none";
+            Novena1sub.style.display = "none";
+            novena1Pic.style.display = "none"
+            N1Button.style.display = "none";
+
+            Novena2.style.display = "none";
+            Novena2sub.style.display = "none";
+            novena2Pic.style.display = "none"
+            N2Button.style.display = "none";
+
+            scrollButtons.style.display = "inline";
+            firstScrollDot.style.opacity = "50%";
+            secondScrollDot.style.opacity = "50%";
+            thirdScrollDot.style.opacity = "100%";
+
+            roses.style.animation = 'roses 7s ease-in-out 7s infinite';
+            roses2.style.animation = 'roses 10s ease-in-out 5s infinite';
+            roses3.style.animation = 'roses 5s ease-in-out 6s infinite';
+            roses4.style.animation = 'roses 10s ease-in-out 1s infinite';
+            roses5.style.animation = 'roses 8s ease-in-out 3s infinite';
+ }
