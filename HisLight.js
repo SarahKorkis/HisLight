751373f0bf3a6e4f25bbbb12d3c109e438f79c61
Sarah Korkis <Sarahkorkis@gmail.com>
2022-09-22 12:43:33 -0400

    Update HisLight.js

--- HisLight.js
+++ HisLight.js
@@ -2,13 +2,13 @@
 
 
 
-
 function N1function() {
     var Novena1 = document.getElementById("Novena1");
     var Novena1A = document.getElementById("Novena1A");
     var N1Button = document.getElementById("N1Button");
     var Novena1sub = document.getElementById("Novena1sub");
     var Novena1subA = document.getElementById("Novena1subA");
+    var scrollButtons = document.getElementById("scrollButtons");
 
     const novena1Day = new Date().toLocaleString().split(',')[0]
 
@@ -26,13 +26,15 @@ function N1function() {
       N1Button.style.top = "570vh";
       Novena1A.style.display = "none";
       Novena1subA.style.display = "none";
+      scrollButtons.style.display = "inline";
     } else {
       Novena1.style.display = "none";
       Novena1sub.style.display = "none";
       N1Button.innerHTML = "Back";
-      N1Button.style.top = "585vh";
+      N1Button.style.top = "600vh";
       Novena1A.style.display = "inline";
       Novena1subA.style.display = "inline";
+      scrollButtons.style.display = "none";
     }
   }
 
@@ -45,6 +47,8 @@ function N1function() {
     var N2Button = document.getElementById("N2Button");
     var Novena2sub = document.getElementById("Novena2sub");
     var Novena2subA = document.getElementById("Novena2subA");
+
+    var N1Button = document.getElementById("N1Button");
 
     const novena2Day = new Date().toLocaleString().split(',')[0]
 
@@ -68,15 +72,77 @@ function N1function() {
       N2Button.style.top = "570vh"
       Novena2A.style.display = "none";
       Novena2subA.style.display = "none";
+      scrollButtons.style.display = "inline";
+      N1Button.style.display = "none"
+      scrollButtons.style.display = "inline";
     } else {
       Novena2.style.display = "none";
       Novena2sub.style.display = "none";
       N2Button.innerHTML = "Back";
-      N2Button.style.top = "585vh"
+      N2Button.style.top = "600vh";
       Novena2A.style.display = "inline";
       Novena2subA.style.display = "inline";
+      scrollButtons.style.display = "none";
+      N1Button.style.display = "none"
+      scrollButtons.style.display = "none";
     }
   }
+
+  function N3function() {
+    var Novena3 = document.getElementById("Novena3");
+    var Novena3A = document.getElementById("Novena3A");
+    var N3Button = document.getElementById("N3Button");
+    var Novena3sub = document.getElementById("Novena3sub");
+    var Novena3subA = document.getElementById("Novena3subA");
+    var scrollButtons = document.getElementById("scrollButtons");
+
+    const novena3Day = new Date().toLocaleString().split(',')[0]
+
+    if (novena3Day == '9/13/2022'){
+      Novena3A.innerHTML = 'Day 8';
+    }
+    else if (novena3Day == '9/14/2022'){
+      Novena3A.innerHTML = 'Day 9';
+    }
+  
+    if (Novena3.style.display === "none") {
+      Novena3.style.display = "inline";
+      Novena3sub.style.display = "inline";
+      N3Button.innerHTML = "Pray";
+      N3Button.style.top = "570vh";
+      Novena3A.style.display = "none";
+      Novena3subA.style.display = "none";
+      scrollButtons.style.display = "inline";
+    } else {
+      Novena3.style.display = "none";
+      Novena3sub.style.display = "none";
+      N3Button.innerHTML = "Back";
+      N3Button.style.top = "600vh";
+      Novena3A.style.display = "inline";
+      Novena3subA.style.display = "inline";
+      scrollButtons.style.display = "none";
+    }
+  }
+
+
+/*    if (Novena2.style.display === "inline") {
+      Novena2.style.display = "inline";
+      Novena2sub.style.display = "inline";
+      Novena1.style.display = "none";
+      N2Button.style.display = "inline";
+      N1Button.style.display = "none";
+      Novena1sub.style.display = "none";
+      scrollButtons.style.display = "inline";
+    } else{
+      Novena2.style.display = "inline";
+      Novena2sub.style.display = "inline";
+      Novena1.style.display = "none";
+      N2Button.style.display = "inline";
+      N1Button.style.display = "none";
+      Novena1sub.style.display = "none";
+      scrollButtons.style.display = "none";
+    }
+  */
 
   if(window.innerWidth <= 400){
     function N1function() {
@@ -85,6 +151,7 @@ function N1function() {
       var N1Button = document.getElementById("N1Button");
       var Novena1sub = document.getElementById("Novena1sub");
       var Novena1subA = document.getElementById("Novena1subA");
+      var scrollButtons = document.getElementById("scrollButtons");
 
       const novena1Day = new Date().toLocaleString().split(',')[0];
 
@@ -99,16 +166,18 @@ function N1function() {
         Novena1.style.display = "inline";
         Novena1sub.style.display = "inline";
         N1Button.innerHTML = "Pray";
-        N1Button.style.top = "980vw";
+        N1Button.style.top = "930vw";
         Novena1A.style.display = "none";
         Novena1subA.style.display = "none";
+        scrollButtons.style.display = "inline";
       } else {
         Novena1.style.display = "none";
         Novena1sub.style.display = "none";
         N1Button.innerHTML = "Back";
-        N1Button.style.top = "1098vw";
+        N1Button.style.top = "1010vw";
         Novena1A.style.display = "inline";
         Novena1subA.style.display = "inline";
+        scrollButtons.style.display = "none";
       }
     }
   
@@ -119,6 +188,7 @@ function N1function() {
       var N2Button = document.getElementById("N2Button");
       var Novena2sub = document.getElementById("Novena2sub");
       var Novena2subA = document.getElementById("Novena2subA");
+      var scrollButtons = document.getElementById("scrollButtons");
 
 
       const novena2Day = new Date().toLocaleString().split(',')[0]
@@ -140,16 +210,18 @@ function N1function() {
         Novena2.style.display = "inline";
         Novena2sub.style.display = "inline";
         N2Button.innerHTML = "Pray";
-        N2Button.style.top = "1225vw"
+        N2Button.style.top = "930vw";
         Novena2A.style.display = "none";
         Novena2subA.style.display = "none";
+        scrollButtons.style.display = "inline";
       } else {
         Novena2.style.display = "none";
         Novena2sub.style.display = "none";
         N2Button.innerHTML = "Back";
-        N2Button.style.top = "1335vw"
+        N2Button.style.top = "1010vw";
         Novena2A.style.display = "inline";
         Novena2subA.style.display = "inline";
+        scrollButtons.style.display = "none";
       }
     }
   }
